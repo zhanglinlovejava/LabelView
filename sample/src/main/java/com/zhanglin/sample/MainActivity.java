6636cb140b943947c5457d5e5039e85f60cc6465
zhanglin <lin.zhang01@bianlifeng.com>
2018-04-23 12:05:02 +0800

    maven

---
+++ sample/src/main/java/com/zhanglin/sample/MainActivity.java
@@ -0,0 +1,25 @@
+package com.zhanglin.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.zhanglin.labelview.view.LabelTextView;
+
+public class MainActivity extends AppCompatActivity {
+    private boolean isShow = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.label).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isShow = !isShow;
+                ((LabelTextView) v).showOrHideLabel(isShow);
+            }
+        });
+    }
+}
+
